3dff3c7264343381612de5bb58a527d4af0ec3a3
Johan Galeano <johans.galeanot@hotmail.com>
2020-09-15 16:27:12 -0500

    peticiones multiples

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,4 +1,5 @@
 let XMLHttpReequest = require('xmlhttprequest').XMLHttpRequest;
+const API = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url_api, callback) {
     let xhttp = new XMLHttpReequest();
@@ -9,9 +10,22 @@ function fetchData(url_api, callback) {
                 callback(null, JSON.parse(xhttp.responseText));
             } else {
                 const error = new Error('Error' + url_api);
-                return callback(error, null)
+                return callback(error, null);
             }
         }
     }
     xhttp.send();
 }
+
+fetchData(API, function (error1, data1) {
+    if (error1) return console.log(error1);
+    fetchData(API + data1.results[0].id, function (error2, data2) {
+        if (error2) return console.log(error2);
+        fetchData(data2.origin.url, function (error3, data3) {
+            if (error3) return console.log(error3);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        })
+    })
+})
